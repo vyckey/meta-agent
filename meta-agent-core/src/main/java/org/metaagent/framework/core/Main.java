b0ba6bca39372401f41e5350f2fc484017b09ab8
vyckey <vyckey@qq.com>
2024-12-28 13:51:02 +0800

    init project

---
+++ meta-agent-core/src/main/java/org/metaagent/framework/core/Main.java
@@ -0,0 +1,12 @@
+package org.metaagent.framework.core;
+
+/**
+ * description is here
+ *
+ * @author vyckey
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/metaagent/framework/Main.java
@@ -0,0 +1,12 @@
+package org.metaagent.framework;
+
+/**
+ * description is here
+ *
+ * @author vyckey
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
